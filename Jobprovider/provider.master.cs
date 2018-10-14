40b9f16c53268ade2a8deb4ccf8eb6f904dc23e2
muneersha <bl.muneersha@gmail.com>
2018-10-14 20:47:21 +0530

    job provider home
    job provider home page added

---
+++ Jobprovider/provider.master.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Web.UI.HtmlControls;
+using System.Xml.Linq;
+
+public partial class Company_cmpmaster : System.Web.UI.MasterPage
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        lb1.Text = Session["cmpuser"].ToString();
+    }
+
+    protected void Button1_Click1(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Company/cmphome.aspx");
+    }
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Company/about.aspx");
+    }
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Company/contact.aspx");
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/jslogin.aspx");
+    }
+    protected void Button5_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Company/addjob.aspx");
+    }
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Company/addvacancy.aspx");
+    }
+    protected void Button7_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Company/interviewsch.aspx");
+    }
+    protected void Button8_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Company/finallist.aspx");
+    }
+    protected void Button9_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Company/Communication.aspx");
+    }
+    protected void Button10_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Company/passchange.aspx");
+    }
+    protected void Button11_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Company/sendinterview.aspx");
+    }
+    protected void Button12_click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Company/Communication.aspx");
+    }
+}
---
+++ Jobprovider/providerhome.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Company/cmpmaster.master" AutoEventWireup="true" CodeFile="cmphome.aspx.cs" Inherits="Company_cmphome" Title="Untitled Page" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
+     <div style=" margin-top:-16.8%; margin-left:-4.4%">
+    <asp:Image ID="Image1" runat="server" ImageUrl="~/images/company.jpg" 
+        Height="575px" Width="1307px" />
+        </div>
+</asp:Content>
+
