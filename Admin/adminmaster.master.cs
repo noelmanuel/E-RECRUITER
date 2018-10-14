4bd600de9b80682b45cc5518453589e8ea9e85e6
muneersha <bl.muneersha@gmail.com>
2018-10-14 12:10:41 +0530

    admin home
    admin homepage added

--- Admin/adminmaster.master.cs
+++ Admin/adminmaster.master.cs
@@ -13,9 +13,51 @@ using System.Xml.Linq;
 
 public partial class Admin_adminmaster : System.Web.UI.MasterPage
 {
-    protected void Page_Load(object sender, EventArgs e)
+    protected void Button1_Click(object sender, EventArgs e)
     {
+        Response.Redirect("~/Admin/adminhome.aspx");
 
     }
-    
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/cs.aspx");
+    }
+
+    protected void Button3_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/contact.aspx");
+    }
+    protected void Button4_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/about.aspx");
+    }
+
+    protected void Button6_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/approvecompany.aspx");
+    }
+    protected void Button7_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/approvejs.aspx");
+    }
+    protected void Button8_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/examsch.aspx");
+    }
+    protected void Button9_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/addquestion.aspx");
+    }
+    protected void Button10_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/presult.aspx");
+    }
+    protected void Button11_Click(object sender, EventArgs e)
+    {
+        Response.Redirect("~/Admin/sendexsch.aspx");
+    }
+    protected void Button5_Click1(object sender, EventArgs e)
+    {
+        Response.Redirect("~/jslogin.aspx");
+    }
 }
